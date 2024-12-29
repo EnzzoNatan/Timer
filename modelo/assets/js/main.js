61b497762128fb1d855961aa600b2640797bbbf0
EnzzoNatan <enzzoferrazz1909@gmail.com>
2024-12-28 22:01:27 -0300

    alterações no cdg

--- modelo/assets/js/main.js
+++ modelo/assets/js/main.js
@@ -9,30 +9,30 @@ let pausar = document.querySelector('.pausar');
 let zerar = document.querySelector('.zerar');
 
 let segundos = 0;
-let timer; // Aqui a variável timer é global
+let timer; 
 
 function iniciaRelogio(){
-    // Aqui a função não precisa redeclarar a variável timer
+    
     timer = setInterval(function(){
-        segundos++; // Incrementa os segundos
+        segundos++; 
         relogio.innerHTML = criaHoraDosSegundos(segundos);        
     }, 1000);
 }
 
 iniciar.addEventListener('click', function(event){
     relogio.classList.remove('pausado');
-    clearInterval(timer); // Para qualquer intervalo existente
-    iniciaRelogio(); // Inicia um novo intervalo
+    clearInterval(timer);
+    iniciaRelogio(); 
 }); 
 
 pausar.addEventListener('click', function(event){
-    clearInterval(timer); // Pausa o relógio ao parar o intervalo
+    clearInterval(timer); 
     relogio.classList.add('pausado');
 });
 
 zerar.addEventListener('click', function(event){
-    clearInterval(timer); // Para o timer
-    segundos = 0; // Reseta os segundos
-    relogio.innerHTML = '00:00:00'; // Reseta o relógio para 00:00:00
+    clearInterval(timer);
+    segundos = 0;
+    relogio.innerHTML = '00:00:00';
     segundos = 0;
 });
